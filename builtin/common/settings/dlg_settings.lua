minetest/minetest

Check for keybinding conflicts

builtin/common/settings/dlg_settings.lua
@@ -22,7 +22,6 @@ local component_funcs =  dofile(path .. "components.lua")
 local shadows_component =  dofile(path .. "shadows_component.lua")
 
 local loaded = false
-local full_settings
 local info_icon_path = core.formspec_escape(defaulttexturedir .. "settings_info.png")
 local reset_icon_path = core.formspec_escape(defaulttexturedir .. "settings_reset.png")
 local all_pages = {}
@@ -32,7 +31,7 @@ local filtered_page_by_id = page_by_id
 
 
 local function get_setting_info(name)
-	for _, entry in ipairs(full_settings) do
+	for _, entry in ipairs(core.full_settings) do
 		if entry.type ~= "category" and entry.name == name then
 			return entry
 		end
@@ -70,7 +69,7 @@ local function load_settingtypes()
 		end
 	end
 
-	for _, entry in ipairs(full_settings) do
+	for _, entry in ipairs(core.full_settings) do
 		if entry.type == "category" then
 			if entry.level == 0 then
 				section = entry.name
@@ -104,7 +103,7 @@ local function load()
 	end
 	loaded = true
 
-	full_settings = settingtypes.parse_config_file(false, true)
+	core.full_settings = settingtypes.parse_config_file(false, true)
 
 	local touchscreen_layout = {
 		query_text = "Touchscreen layout",

grorp/luanti

builtin/common/settings/dlg_settings.lua
@@ -22,7 +22,6 @@ local component_funcs =  dofile(path .. "components.lua")
 local shadows_component =  dofile(path .. "shadows_component.lua")
 
 local loaded = false
-local full_settings
 local info_icon_path = core.formspec_escape(defaulttexturedir .. "settings_info.png")
 local reset_icon_path = core.formspec_escape(defaulttexturedir .. "settings_reset.png")
 local all_pages = {}
@@ -32,7 +31,7 @@ local filtered_page_by_id = page_by_id
 
 
 local function get_setting_info(name)
-	for _, entry in ipairs(full_settings) do
+	for _, entry in ipairs(core.full_settingtypes) do
 		if entry.type ~= "category" and entry.name == name then
 			return entry
 		end
@@ -70,7 +69,7 @@ local function load_settingtypes()
 		end
 	end
 
-	for _, entry in ipairs(full_settings) do
+	for _, entry in ipairs(core.full_settingtypes) do
 		if entry.type == "category" then
 			if entry.level == 0 then
 				section = entry.name
@@ -104,7 +103,7 @@ local function load()
 	end
 	loaded = true
 
-	full_settings = settingtypes.parse_config_file(false, true)
+	core.full_settingtypes = settingtypes.parse_config_file(false, true)
 
 	local touchscreen_layout = {
 		query_text = "Touchscreen layout",